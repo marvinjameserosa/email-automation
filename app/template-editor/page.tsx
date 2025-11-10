8a240165f4a7ce37604bdd92213937ac18a2254e
ezekielgbustamante@iskolarngbayan.pup.edu.ph <ezekielgbustamante@iskolarngbayan.pup.edu.ph>
2025-11-10 15:30:42 +0800

    feat: create TemplateEditorPage component to render TemplateEditor

---
+++ app/template-editor/page.tsx
@@ -0,0 +1,9 @@
+import TemplateEditor from '../../components/TemplateEditor';
+
+export default function TemplateEditorPage() {
+    return (
+        <main className="p-4">
+            <TemplateEditor />
+        </main>
+    );
+}
